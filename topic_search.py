1b11ad3609e169c9bcc0ce5df5eaf07f4842d17c
FrInve <francesco1.invernici@mail.polimi.it>
2023-05-14 22:13:00 +0200

    first commit

---
+++ topic_search.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pandas as pd
+from bertopic import BERTopic
+import seaborn as sns
+import matplotlib.pyplot as plt
+from src.visualization.timeline import *
+from src.visualization.wc import create_wordcloud
+
+st.set_page_config(layout="wide")
+
+@st.cache_data
+def load_cases_data():
+    df_covid_cases = get_covid_data(end_date='2022-06-15')
+    return df_covid_cases
+
+@st.cache_data
+def load_timeline_data():
+    covid_timeline = pd.read_csv('./data/raw/macmillan_covid.csv')
+    covid_timeline['Date'] = pd.to_datetime(covid_timeline['Date'])
+    covid_timeline.set_index("Date",inplace=True)
+    return covid_timeline
+
+@st.cache_data
+def load_topic_data():
+    df_norm = pd.read_csv('./data/processed/topics_freq_pivoted.csv')
+    df_norm['Date'] = pd.to_datetime(df_norm['Date'])
+    df_norm = df_norm.set_index('Date')
+    return df_norm
+
+@st.cache_resource
+def load_topic_model():
+    topic_model = BERTopic.load('./models/BERTopic_full_2023-04-18',embedding_model="pritamdeka/BioBERT-mnli-snli-scinli-scitail-mednli-stsb")
+    return topic_model
+
+@st.cache_resource
+def create_wordcloud_static(topic):
+    return create_wordcloud(topic_model, topic).to_image()
+
+
+df_covid_cases = load_cases_data()
+covid_timeline = load_timeline_data()
+df_norm = load_topic_data()
+topic_model = load_topic_model()
+
+st.title("CORToViz")
+st.header("The CORD-19 Topics Visualizer")
+
+wcol1, wcol2 = st.columns(2)
+
+with wcol1:
+    query = st.text_input("Search a topic:", value='variant', max_chars=100)
+
+    similar_topic, similarity = topic_model.find_topics(query, top_n=6)
+    #st.text("Topic\tSimilarity")
+    #for topic, sim in zip(similar_topic, similarity):
+    #    st.text(f"{topic}\t{sim}")
+
+    st.subheader("Inspect Topics' WordClouds:")
+
+    col1, col2, col3 = st.columns(3)
+    columns = [col1,col2,col3]
+
+    selecting_topics = {str(topic):True for topic in similar_topic}
+
+
+    for i, topic in enumerate(similar_topic):
+        with columns[i%3]:
+            st.text(f"{topic_model.get_topic(topic)[0][0]}")
+            st.image(create_wordcloud_static(topic))
+            chosen_val = st.checkbox(str(topic),value=True)
+            selecting_topics[str(topic)] = chosen_val
+            st.text("------------------")
+        
+
+    selected_topics = [topic[0] for topic in selecting_topics.items() if topic[1]]
+
+with wcol2:
+    st.text(f"Currently showing {len(selected_topics)} topics over time")
+
+    df_tmp = df_norm[df_norm.index < '2022-06-06'][selected_topics].copy(deep=True)
+    fig, ax1 = plt.subplots(figsize=(11.7,8.27))
+    #sns.set_theme()
+    #plt.stackplot(df_tmp.index, df_tmp.iloc[:,0] )
+    sns.lineplot(data=df_tmp, dashes=False).set(title=query)
+    sns.histplot(data=df_tmp,x=df_tmp.index).set(title=query)
+    ax2= ax1.twinx()
+    plot_events(covid_timeline[covid_timeline.Included==1][['Event']].to_dict()['Event'])
+    plot_covid_cases(covid_df=df_covid_cases, ax=ax2)
+    st.pyplot(fig)
+
+st.markdown("Copyright (C) 2023 Francesco Invernici, All Rights Reserved")
